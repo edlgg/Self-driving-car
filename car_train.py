40ca1bf8e5f0a6284820aaf68932f3c8c396de03
Eduardo de la Garza <delagarzaguerra@gmail.com>
2018-03-10 20:54:51 -0600

    changed dir

--- car_train.py
+++ car_train.py
@@ -12,19 +12,19 @@ while p == 0:
     key_press = input('')  # previously raw_input()
 
     if key_press == 'w':
-        os.chdir('/home/pi/Documents/Self-driving-car/images/w')
+        os.chdir('/home/pi/Documents/Self-driving-car-all/images/w')
         camera.take_picture()
         movement.forward(tf)
     elif key_press == 'd':
-        os.chdir('/home/pi/Documents/Self-driving-car/images/d')
+        os.chdir('/home/pi/Documents/Self-driving-car-all/images/d')
         camera.take_picture()
         movement.right(tf)
     elif key_press == 'a':
-        os.chdir('/home/pi/Documents/Self-driving-car/images/a')
+        os.chdir('/home/pi/Documents/Self-driving-car-all/images/a')
         camera.take_picture()
         movement.left(tf)
     elif key_press == 't':
-        os.chdir('/home/pi/Documents/Self-driving-car/images/test')
+        os.chdir('/home/pi/Documents/Self-driving-car-all/images/test')
         camera.take_picture()
     elif key_press == 'p':
         p = 1
