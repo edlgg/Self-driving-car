93cbbb222e783ee316e2d8f7356f420564ca47a2
Eduardo de la Garza <delagarzaguerra@gmail.com>
2018-03-10 20:49:00 -0600

    changed name from self to Self for new git directory

--- camera.py
+++ camera.py
@@ -1,28 +1,32 @@
 import time
 import picamera
 
+
 def init():
-	global camera
-	camera = picamera.PiCamera()
-	camera.resolution = (128,128)
-	camera.start_preview()
-	time.sleep(3)
-	camera.vflip = True
-	camera.hflip = True
-	camera.brightness = 60
+    global camera
+    camera = picamera.PiCamera()
+    camera.resolution = (128, 128)
+    camera.start_preview()
+    time.sleep(3)
+    camera.vflip = True
+    camera.hflip = True
+    camera.brightness = 60
+
 
 def end():
-	camera.stop_preview()
-	camera.close()
+    camera.stop_preview()
+    camera.close()
+
 
 def take_picture():
-	current_date = (time.strftime("%d-%m-%Y"))
-	current_time = (time.strftime("%H:%M:%S"))
-	image_name = current_date + "_" + current_time + ".jpg"
-	camera.capture(image_name)
+    current_date = (time.strftime("%d-%m-%Y"))
+    current_time = (time.strftime("%H:%M:%S"))
+    image_name = current_date + "_" + current_time + ".jpg"
+    camera.capture(image_name)
+
 
 def take_picture_test():
-	image_name = "test.jpg"
-	camera.capture(image_name)
+    image_name = "test.jpg"
+    camera.capture(image_name)
 
 
