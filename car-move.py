bd1471c89eb27d4f5055b91410b0cd18767c43de
Eduardo de la Garza <delagarzaguerra@gmail.com>
2018-03-10 20:17:08 -0600

    fixed movement tabs

--- car-move.py
+++ car-move.py
@@ -3,15 +3,15 @@ import movement as m
 p = 0
 tf = 1.6
 while p == 0:
-	a = raw_input('')
+        a = raw_input('')
 
-	if a == 'w':
-		m.forward(tf)
+        if a == 'w':
+                m.forward(tf)
         if a == 's':
                 m.reverse(tf)
         if a == 'd':
                 m.right(tf)
         if a == 'a':
                 m.left(tf)
-	if a == 'p':
-		p = 1
+        if a == 'p':
+                p = 1
