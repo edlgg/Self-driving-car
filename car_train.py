361d2a9b5576e94220bf50c414219b5ca9ee5543
Eduardo de la Garza <delagarzaguerra@gmail.com>
2018-03-11 16:53:21 -0600

    added camera exception to prevent crash

--- car_train.py
+++ car_train.py
@@ -13,19 +13,35 @@ while p == 0:
 
     if key_press == 'w':
         os.chdir('/home/pi/Documents/Self-driving-car-all/images/w')
-        camera.take_picture()
+        try:
+            camera.take_picture()
+        except Exception:
+            camera.close()
+            camera.init()
         movement.forward(tf)
     elif key_press == 'd':
         os.chdir('/home/pi/Documents/Self-driving-car-all/images/d')
-        camera.take_picture()
+        try:
+            camera.take_picture()
+        except Exception:
+            camera.close()
+            camera.init()
         movement.right(tf)
     elif key_press == 'a':
         os.chdir('/home/pi/Documents/Self-driving-car-all/images/a')
-        camera.take_picture()
+        try:
+            camera.take_picture()
+        except Exception:
+            camera.close()
+            camera.init()
         movement.left(tf)
     elif key_press == 't':
         os.chdir('/home/pi/Documents/Self-driving-car-all/images/test')
-        camera.take_picture()
+        try:
+            camera.take_picture()
+        except Exception:
+            camera.close()
+            camera.init()
     elif key_press == 'p':
         p = 1
         camera.end()
